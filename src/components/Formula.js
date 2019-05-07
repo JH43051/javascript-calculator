ed70403deb1021a0996f6bc04e24586d7799deee
John Holler <jh43051@gmail.com>
2019-05-07 11:44:50 -0400

    Delete Formula.js

--- src/components/Formula.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Formula extends React.Component {
-	render() {
-		return <div className="formulaScreen">{this.props.formula}</div>
-	}
-};
-
-export default Formula;
