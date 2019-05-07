8c83734788a285ed42601845fad0e1e49ab01cd3
John Holler <jh43051@gmail.com>
2019-05-07 11:44:43 -0400

    Delete Output.js

--- src/components/Output.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Output extends React.Component {
-	render() {
-		return <div id="display" className="outputScreen">{this.props.currentValue}</div>
-	}
-};
-
-export default Output;
